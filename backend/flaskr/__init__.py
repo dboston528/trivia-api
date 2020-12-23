32a40e627d9da203e131913b5d5fc657f40f77ac
DeAndre Boston <dboston3@yahoo.com>
2020-12-23 08:26:24 -0600

    Fix CORS and add categories endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -16,17 +16,32 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-  CORS(app, resources={r"*/api/*": {origins: '*'}})
+
+  cors = CORS(app, resources={r"/*/": {"origins": "*"}})
+
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
-
+  @app.route('/categories')
+  def get_categories():
+    thecategories = Category.query.all()
+    formatted_categories =  [category.format() for category in thecategories]
+    print(formatted_categories)
+    return jsonify({
+      'success:': True,
+      'categories': formatted_categories
+    })
 
   '''
   @TODO: 
