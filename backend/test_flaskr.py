be492b441669ddf1d758ec18d8477a3e63ac7bec
DeAndre Boston <dboston3@yahoo.com>
2021-01-06 00:06:33 -0600

    Add unit test for GET categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,14 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def getCategories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['categories'], True)
+
+
 
 
 # Make the tests conveniently executable
